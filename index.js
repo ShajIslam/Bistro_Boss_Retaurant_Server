4ab791fe78b0dd9f8ea99105a0275cd27a9e6a83
Shaj Islam <emamulislamshaj0@gmail.com>
2024-01-03 22:30:29 +0600

    rearrenged code

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const cors = require('cors');
-const app = express();
 require('dotenv').config()
+const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const app = express();
 const port = process.env.PORT || 8001;
 
 // middleware
@@ -10,6 +11,23 @@ const port = process.env.PORT || 8001;
 app.use(express.json());
 app.use(cors());
 
+//middleware
+const verifyToken = (req, res, next)=>{
+  console.log('inside verify token', req.headers);
+
+  if(!req.headers.authorization){
+   return res.status(401).send({message: 'forbidden access'})
+  }
+
+  const token = req.headers.authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+   if(err){
+     return res.status(401).send({message: 'forbidden access'})
+   }
+   req.decoded = decoded;
+   next();
+  })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1vqmal2.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -22,16 +40,25 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function run() {
+const dbConnect = async () => {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
+      client.connect()
+      console.log('DB Connected Successfully✅')
+  } catch (error) {
+      console.log(error.name, error.message)
+  }
+}
+dbConnect();
 
     const foodCollection = client.db('bistroBossDb').collection('menu');
     const reviewsCollection = client.db('bistroBossDb').collection('reviews');
     const cartCollection = client.db('bistroBossDb').collection('myCart');
     const usersCollection = client.db('bistroBossDb').collection('users')
+
+    //default route
+    app.get('/', (req, res)=>{
+      res.send("Server Running Successfully...");
+  })
 
     app.post('/users', async(req, res)=>{
       const user = req.body;
@@ -45,7 +72,6 @@ async function run() {
         res.send(result)
     })
     app.get('/reviews', async(req, res)=>{
-
         const result = await reviewsCollection.find().toArray();
         res.send(result)
     })
@@ -63,32 +89,51 @@ async function run() {
       res.send(result);
       })
 
+      app.get('/users', async(req, res)=>{
+        console.log(req.headers);
+        const result = await usersCollection.find().toArray();
+        res.send(result);
+      })
+
 
       app.delete('/carts/:id', async(req, res)=>{
         const id = req.params.id;
-        console.log('id', id);
         const query = {_id: new ObjectId(id)};
         const result = await cartCollection.deleteOne(query);
         res.send(result);
       })
 
-     
+      app.delete('/users/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await usersCollection.deleteOne(query);
+        res.send(result);
+      })
 
+      //jwt related api
 
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
-}
-run().catch(console.dir);
+      app.post('/jwt', async(req, res)=>{
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1hr'});
+        res.send({token})
 
-
-app.get('/', (req, res)=>{
-    res.send("Server Running Successfully...");
-})
+      })
+      app.patch('/users/admin/:id', async(req, res)=>{
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)}
+        const updatedDoc = {
+          $set:{
+            role: 'admin'
+          }
+        }
+        const result = await usersCollection.updateOne(filter, updatedDoc)
+        res.send(result);
+      })
 
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
 })
+
+//this line is for numberic alue
+//running this sentece successfully 
+//it is the fascination about those things which is allright for sur
